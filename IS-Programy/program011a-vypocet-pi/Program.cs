162e708a7ca628ad9e0c9916905435b541127d32
Linda Kastlová <linda.kastlova@tul.cz>
2025-12-04 16:48:55 +0100

    #13 dokončení programu pro výpočet pí

--- IS-Programy/program011a-vypocet-pi/Program.cs
+++ IS-Programy/program011a-vypocet-pi/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("****** Konvergetní řady *******");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 04.12.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+    
+    Console.Write("Zadejte přesnost: ");
+    double presnost;
+    while (!double.TryParse(Console.ReadLine(), out presnost))
+        Console.Write("Nezadali jste číslo. Zadejte číslo znovu: ");
+
+    double i = 1;
+    double piCtvrt = 1;
+    double znamenko = 1;
+
+    while (1 / i >= presnost)
+    {
+        i = i + 2;
+        znamenko = -znamenko;
+        piCtvrt = piCtvrt + znamenko * 1 / i;
+    }
+    double pi = piCtvrt * 4;
+    Console.WriteLine("Zobrazení pí:{0}", pi);
+    
+    
+
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
