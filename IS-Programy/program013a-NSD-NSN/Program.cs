f6ea14ec892500ae948b0dc1b9f6346e2df7a3cb
Linda Kastlová <linda.kastlova@tul.cz>
2025-12-09 23:05:21 +0100

    #15 začátek úkolu

--- IS-Programy/program013a-NSD-NSN/Program.cs
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -1,3 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿var again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("*******************************");
+    Console.WriteLine("********** NSD a NSN **********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine("******** Linda Kastlová *******");
+    Console.WriteLine("********** 09.12.2025 *********");
+    Console.WriteLine("*******************************");
+    Console.WriteLine();
 
-Console.WriteLine("Hello, World!");
+
+    // Vstup číselné hodnoty do programu 
+    Console.Write("Zadejte první celé číslo: ");
+    ulong numberA;
+    while (!ulong.TryParse(Console.ReadLine(), out numberA))
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    
+    Console.Write("Zadejte druhé celé číslo: ");
+    ulong numberB;
+    while (!ulong.TryParse(Console.ReadLine(), out numberB))
+        Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
+    
+    Console.WriteLine("První zadané číslo: {0}, druhé zadané číslo: {1}",  numberA, numberB);
+
+    ulong A = numberA;
+    ulong B = numberB;
+
+    for (A <> B)
+    {
+       for(A > B) 
+           
+    }
+    
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+}
