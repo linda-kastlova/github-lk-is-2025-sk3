8ae8679bca77e2e6df073d8b632f8baec62e7b73
Linda Kastlová <linda.kastlova@tul.cz>
2025-12-10 18:33:46 +0100

    #15 dokončení programu

--- IS-Programy/program013a-NSD-NSN/Program.cs
+++ IS-Programy/program013a-NSD-NSN/Program.cs
@@ -13,27 +13,43 @@ while (again == "a")
 
     // Vstup číselné hodnoty do programu 
     Console.Write("Zadejte první celé číslo: ");
-    ulong numberA;
-    while (!ulong.TryParse(Console.ReadLine(), out numberA))
+    ulong inputFirst;
+    while (!ulong.TryParse(Console.ReadLine(), out inputFirst))
         Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
     
     Console.Write("Zadejte druhé celé číslo: ");
-    ulong numberB;
-    while (!ulong.TryParse(Console.ReadLine(), out numberB))
+    ulong inputSecond;
+    while (!ulong.TryParse(Console.ReadLine(), out inputSecond))
         Console.Write("Nezadali jste celé číslo. Zadejte číslo znovu: ");
     
-    Console.WriteLine("První zadané číslo: {0}, druhé zadané číslo: {1}",  numberA, numberB);
+    Console.WriteLine("První zadané číslo: {0}, druhé zadané číslo: {1}",  inputFirst, inputSecond);
 
-    ulong A = numberA;
-    ulong B = numberB;
-
-    for (A <> B)
-    {
-       for(A > B) 
-           
-    }
+    int iterations = 0;
+    ulong first = inputFirst;
+    ulong second = inputSecond;
     
+    while (first != second)
+    {
+        iterations++;
+        
+        if (first > second)
+        {
+            first = first - second;
+            Console.WriteLine("Krok {0}: První číslo {1} - druhé číslo {2}", iterations, first, second);
+        }
+        else
+        {
+            second = second - first;
+            Console.WriteLine("Krok {0}: Druhé číslo {1} - první číslo {2}", iterations, second, first);
+        }
+    }    
+    Console.WriteLine("=============================================");
+    Console.WriteLine("Největší společný dělitel je číslo {0}", first);
 
+    ulong leastCommonMultiple = (inputFirst * inputSecond) / first;
+        
+    Console.WriteLine("Nejmenší společný násobek je {0}", leastCommonMultiple);
+    
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
     again = Console.ReadLine();
